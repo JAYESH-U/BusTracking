280c468d7af77d6a7b5f0f1ba200afbca3c25d22
Jayesh <uramanattijayesh@gmail.com>
2023-07-14 20:18:18 +0530

    bus no

--- app.js
+++ app.js
@@ -270,7 +270,7 @@ app.post("/map", function (req, res) {
 //endpoint to fetch the foundBus data through AJAX
 app.get("/selectedbus", function (req, res) {
     console.log("trying to fetch");
-    const busNo = selectedBus.busNo;
+    const busNo = null;
     if (selectedBus === null) {
         busNo = 1;
     }

fc570b383e1e3e0acd9a9272a42680091655d566
Jayesh <uramanattijayesh@gmail.com>
2023-07-30 20:17:59 +0530

    mongo URI

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ const MongoURI = 'mongodb://127.0.0.1:27017/busTrackDB';
 
 // process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/busTrackDB'  
 mongoose
-    .connect(MongoURI, {
+    .connect(process.env.MONGO_URI, {
         useNewUrlParser: true,
         useUnifiedTopology: true
     })
@@ -43,7 +43,7 @@ mongoose
     });
 
 const store = new MongoDBSession({
-    uri: MongoURI,
+    uri: process.env.MONGO_URI,
     collection: "mySessions",
 });
 

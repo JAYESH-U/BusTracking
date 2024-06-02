8bd8e7e34b613eb4d83d1bbb711881423c2f5f00
Jayesh <uramanattijayesh@gmail.com>
2024-06-02 23:52:32 +0530

    public path change

--- app.js
+++ app.js
@@ -19,7 +19,8 @@ const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-app.use(express.static("public"));
+// app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
